3c8f172f1b13717380347a34be9bc7eb4a0743bb
Michael <88594241+NewLifeWorks@users.noreply.github.com>
2022-03-26 03:22:55 -0500

    Add files via upload

---
+++ AGC Duluth/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#slides').superslides( {
+        animation: 'fade', play: '5000', pagination: 'false'
+    });
+
+    var typed = new Typed(".typed", {
+        strings: ["Acts 2:38", "Receive the promise of the Holy Ghost", "Worship", "Put on the whole armor of God", "Praise"], typeSpeed: 50,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
+
+});
